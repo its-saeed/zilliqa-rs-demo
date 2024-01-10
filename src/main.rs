161e18f411a94d2072dc5592487cc8ff1cd7cd21
Saeed Dadkhah <saeed@zilliqa.com>
2024-01-10 08:18:01 +0330

    Add contract deployment doc

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,32 @@
 use std::error::Error;
 
+use zilliqa_rs::contract::HelloWorld;
 use zilliqa_rs::middlewares::Middleware;
 use zilliqa_rs::providers::{Http, Provider};
+use zilliqa_rs::signers::LocalWallet;
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn Error>> {
-    let provider = Provider::<Http>::try_from("http://127.0.0.1:5555")?.with_chain_id(222);
+    let wallet = "0xe53d1c3edaffc7a7bab5418eb836cf75819a82872b4a1a0f1c7fcf5c3e020b89"
+        .parse::<LocalWallet>()?;
+
+    let provider = Provider::<Http>::try_from("http://127.0.0.1:5555")?
+        .with_chain_id(222)
+        .with_signer(wallet.clone());
+
     let balance = provider
         .get_balance("0x381f4008505e940ad7681ec3468a719060caf796")
         .await;
 
     println!("{balance:?}");
+
+    let contract = HelloWorld::deploy(provider.into(), wallet.address).await?;
+    println!("Contract address: {:?}", contract.address());
+
+    println!("Contract owner: {:?}", contract.owner().await?);
+    println!("Welcome msg: {}", contract.welcome_msg().await?);
+
+    contract.set_hello("Salaam".to_string()).call().await?;
+    println!("Welcome msg: {}", contract.welcome_msg().await?);
     Ok(())
 }
